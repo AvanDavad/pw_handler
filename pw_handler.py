17535e25119ddb680dcbbc8212adcb43b16533a9
Ivan, David <avandavad@gmail.com>
2022-10-16 21:05:24 +0200

    read does not crash

--- pw_handler.py
+++ pw_handler.py
@@ -20,7 +20,7 @@ class PasswordHandler:
         self._get_password()
         self._load()
         self._start()
-        
+
     def _init_commands(self):
         self.COMMANDS = [
             (self._quit, ['q', 'q!', 'wq', 'sq'], 'quit'),
@@ -35,7 +35,7 @@ class PasswordHandler:
             (self._change_key, ['change'], 'change the password'),
             (self._help, ['h', 'help'], 'show this help message')
         ]
-        
+
     def _get_password(self):
         x = getpass()
         x_list = x.split('.')
@@ -46,19 +46,19 @@ class PasswordHandler:
                       AES.MODE_CBC, 
                       bytes([0 for _ in range(16)]))
         self.key = obj.encrypt(prekey)
-        
+
     def _load(self):
         self._read_file()
         self.content = self._decrypt()
         self._check_content()
         self._refresh_content_lines()
-        
+
     def _check_content(self):
         for b in self.content:
             if b>128:
                 print('Content is corrupted.')
                 break
-    
+
     def _read_file(self):
         if not os.path.isfile(self.filename):
             print('no file {}. initializing new one'.format(self.filename))
@@ -66,17 +66,17 @@ class PasswordHandler:
         else:
             with open(self.filename, 'rb') as f:
                 self.ciphertext = bytearray(f.read())
-    
+
     def _refresh_content_lines(self):
         self.content_lines = self.content.split(b'\n')
         self.content_lines = [line for line in self.content_lines if len(line) > 0]
-    
+
     def _start(self):
         while True:
             inp = input('>')
             if inp == '':
                 continue
-            
+
             x0 = inp.split()[0]
             params = inp.split()[1:]
             for fun, command, _ in self.COMMANDS:
@@ -86,11 +86,11 @@ class PasswordHandler:
             else:
                 print('invalid command: {}'.format(x0))
                 print('enter h or help for list of commands!')
-    
+
     def _help(self, x0=None, params=None):
         for _, command, help_msg in self.COMMANDS:
             print('{}: {}'.format(' or '.join(command), help_msg))
-        
+
     def _delete_all(self, x0=None, params=None):
         x = input('delete all rows? (y/n)')
         if x=='y':
@@ -101,7 +101,7 @@ class PasswordHandler:
             print('not deleting all rows.')
         else:
             print('enter either y or n.')
-    
+
     def _delete_row(self, x0=None, params=None):
         row = self._get_first(params)
         row = self._validate_row(row)
@@ -110,7 +110,7 @@ class PasswordHandler:
         print('deleted:{}'.format(self.content_lines[row].decode()))
         self.content_lines = self.content_lines[:row] + self.content_lines[row+1:]
         self.content = b'\n'.join(self.content_lines)
-    
+
     def _validate_row(self, row):
         try:
             r = int(row)
@@ -121,7 +121,7 @@ class PasswordHandler:
             print('please enter a valid row number!')
             return
         return r
-    
+
     def _find(self, x0=None, params=None):
         regex = self._get_first(params)
         regex = re.compile(regex, re.IGNORECASE)
@@ -129,7 +129,7 @@ class PasswordHandler:
             s = regex.search(line.decode())
             if s is not None:
                 self._print_line(i)
-    
+
     def _quit(self, x0, params=None):
         if x0 == 'q':
             save = input('save changes (y/n)?:')
@@ -144,15 +144,15 @@ class PasswordHandler:
             return self._force_quit()
         if x0 in ['wq', 'sq']:
             return self._save_quit()
-    
+
     def _force_quit(self):
         print('not saving changes')
         sys.exit()
-    
+
     def _save_quit(self):
         self._save()
         sys.exit()
-    
+
     def _write(self, x0=None, params=None):
         msg = ' '.join(params)
         try:
@@ -162,29 +162,32 @@ class PasswordHandler:
                 self.content = b'\n'.join(self.content_lines)
         except:
             print('something went wrong.. please use only ascii!')
-    
+
     def _read(self, x0=None, params=None):
-        st_row = self._get_first(params)
-        st_row = 0 if st_row=='' else int(st_row)
-        end_row= self._get_second(params)
-        end_row=len(self.content_lines) if end_row=='' else int(end_row) 
-        for i, line in enumerate(self.content_lines):
-            if (i>=st_row) and (i<end_row):
-                self._print_line(i)
-    
+        try:
+            st_row = self._get_first(params)
+            st_row = 0 if st_row=='' else int(st_row)
+            end_row= self._get_second(params)
+            end_row=len(self.content_lines) if end_row=='' else int(end_row)
+            for i, line in enumerate(self.content_lines):
+                if (i>=st_row) and (i<end_row):
+                    self._print_line(i)
+        except:
+            print("could not read file.")
+
     def _get_first(self, x_list, default=''):
         if len(x_list)==0:
             return default
         return x_list[0]
-    
+
     def _get_second(self, x_list, default=''):
         if len(x_list)<2:
             return default
         return x_list[1]
-    
+
     def _print_line(self, i):
         print('{}:{}'.format(i, self.content_lines[i].decode()))
-    
+
     def _generate_password(self, x0=None, params=None):
         k = self._get_first(params)
         k = int(k) if len(k)>0 else 8
@@ -203,7 +206,7 @@ class PasswordHandler:
                 if s is None:
                     good_pwd = False
         print(pwd)
-        
+
     def _decrypt(self):
         if len(self.ciphertext) == 0:
             return bytes([])
@@ -214,27 +217,25 @@ class PasswordHandler:
         if 255 in x:
             x = x[:x.index(255)]
         return x
-    
+
     def _clear(self, x0=None, params=None):
         os.system('clear')
-    
+
     def _save(self, x0=None, params=None):
         iv, cont = self._encrypt()
         with open(self.filename, 'wb') as f:
             f.write(iv+cont)
         print('saved {}'.format(self.filename))
-    
+
     def _encrypt(self):
         iv = bytes([random.randint(0,255) for _ in range(16)])
         obj = AES.new(self.key, AES.MODE_CBC, iv)
         rem = (-len(self.content)) % 16
         self.content = self.content + bytes([255 for _ in range(rem)])
         return (iv, obj.encrypt(self.content))
-    
+
     def _change_key(self, x0=None, params=None):
         self._get_password()
 
 if __name__ == '__main__':
     pwh = PasswordHandler(args.input_file)
-    
-    
